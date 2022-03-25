3a2d0a24963cae42bb9b9b6ffbc73376c4c36df6
antor10655gh <creativefreelancerantor@gmail.com>
2022-03-26 05:25:28 +0600

    trash button is work

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,17 +3,17 @@ import './Cart.css'
 
 const Cart = (props) => {
     // console.log(props);
-    const {picture, name} = props.cart;
+    const {id, picture, name} = props.cart;
     const removeCart = (id) =>{
-        
+        document.getElementById(id).textContent = '';
     }
     return (
         <div className='row'>
             <div className="col">
-                <div className='cart-items text-start mb-3'>
+                <div className='cart-items text-start mb-3' id={id}>
                     <img className='slt-product' src={picture} alt="" />    
                     <span>{name}</span>
-                    <button onClick={()=>removeCart(props.cart)} className='trash-btn'>
+                    <button onClick={()=>removeCart(id)} className='trash-btn'>
                         <i className="fa-solid fa-trash trash-btn"></i>
                     </button>
                 </div>
